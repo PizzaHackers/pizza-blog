0b9d94a4a321bef851a2babea5e2dd4cd076dfd6
Sanket Saurav <sanketsaurav@gmail.com>
2014-02-12 02:31:00 +0530

    Trying to fix pg.

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ config = {
         database: {
             client: 'postgres',
             connection: {
-		    host: process.env.POSTGRES_HOST,
+		    host: process.env.DATABASE_URL,
 		    user: process.env.POSTGRES_USER,
 		    password: process.env.POSTGRES_PASSWORD,
 		    database: process.env.POSTGRES_DATABASE,
